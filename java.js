ce730060622de0d228c7944e1bc5e17f96b238bb
onifernando1 <onifernando1@gmail.com>
2022-06-07 17:13:03 +0100

    Move css to styles css instead of embedding in java

--- java.js
+++ java.js
@@ -2,6 +2,5 @@ const base = document.querySelector(".base")
 
 const div = document.createElement("div");
 div.classList.add("grid")
-div.setAttribute("style", "color: blue; background:green;")
 base.appendChild(div)
 

27ca73195231a905e0fbd03b6e0e9b901cf6f4de
onifernando1 <onifernando1@gmail.com>
2022-06-07 21:24:50 +0100

    Change div size to smaller

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 const base = document.querySelector(".base")
 
-for (i =0; i <10;i++){
+for (i =0; i < 256;i++){
     const div = document.createElement("div");
     div.classList.add("grid")
     base.appendChild(div)

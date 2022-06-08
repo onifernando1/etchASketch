403b4c3ec4641327f0c38c30885dea48df0707d4
onifernando1 <onifernando1@gmail.com>
2022-06-08 17:22:42 +0100

    Create drawgrid() function

--- java.js
+++ java.js
@@ -1,23 +1,25 @@
 const base = document.querySelector(".base")
 let gridSize = 256
 
+drawGrid = function(){
+    for (i =0; i < gridSize;i++){
+        const div = document.createElement("div");
+        div.classList.add("grid")
+        base.appendChild(div)
+    }
 
-for (i =0; i < gridSize;i++){
-    const div = document.createElement("div");
-    div.classList.add("grid")
-    base.appendChild(div)
+    const grid = document.querySelectorAll(".grid")
+    grid.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.classList.add("change")
+            const change = document.querySelectorAll(".change")
+            change.forEach((initialSquare) => {
+                initialSquare.setAttribute("style", "background:black;")
+            })
+        });
+    });
 }
 
-const grid = document.querySelectorAll(".grid")
-grid.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("change")
-        const change = document.querySelectorAll(".change")
-        change.forEach((initialSquare) => {
-            initialSquare.setAttribute("style", "background:black;")
-        })
-    });
-});
-
+drawGrid()
 const button = document.querySelector(".button")
-button.onclick = () => gridSize = prompt("What size grid would you like? ") 
+button.onclick = () => gridSize = prompt("What size grid would you like? ");
